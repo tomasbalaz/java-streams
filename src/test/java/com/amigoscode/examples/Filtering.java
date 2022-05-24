a03db04c6669101b91b81d4ab85eca03cfa01e0c
Tomas Balaz <tomasba@amdocs.com>
2022-05-24 14:18:36 +0200

    Example of how to use filter, dropWhile, takeWhile, findFirst, findAny, allMatch, anyMatch

--- src/test/java/com/amigoscode/examples/Filtering.java
+++ src/test/java/com/amigoscode/examples/Filtering.java
@@ -17,46 +17,79 @@ public class Filtering {
     @Test
     public void filter() throws Exception {
         List<Car> cars = MockData.getCars();
+
+        Predicate<Car> carPredicate = car -> car.getPrice() < 20_000.00;
+        Predicate<Car> yellow = car -> car.getColor().equals("Yellow");
+        List<Car> carsLessThan20k = cars.stream()
+                .filter(carPredicate)
+                .filter(yellow)
+                .toList();
+        carsLessThan20k.forEach(System.out::println);
     }
 
     @Test
     public void dropWhile() throws Exception {
+        // using filter
         System.out.println("using filter");
-        Stream.of(2, 4, 6, 8, 9, 10, 12).filter(n -> n % 2 == 0)
+        Stream.of(2, 4, 6, 8, 9, 10, 12)
+                .filter(n -> n % 2 == 0)
                 .forEach(n -> System.out.print(n + " "));
-        System.out.println();
-        System.out.println("using dropWhile");
-    }
 
+        // using takeWhile
+        System.out.println("\nusing dropWhile");
+        Stream.of(2, 4, 6, 8, 9, 10, 12)
+                .dropWhile(n -> n % 2 == 0)
+                .forEach(n -> System.out.print(n + " "));
+    }
     @Test
     public void takeWhile() throws Exception {
         // using filter
         System.out.println("using filter");
-        Stream.of(2, 4, 6, 8, 9, 10, 12).filter(n -> n % 2 == 0)
+        Stream.of(2, 4, 6, 8, 9, 10, 12)
+                .filter(n -> n % 2 == 0)
                 .forEach(n -> System.out.print(n + " "));
 
-        System.out.println();
-        System.out.println("using take while");
+        // using takeWhile
+        System.out.println("using takeWhile");
+        Stream.of(2, 4, 6, 8, 9, 10, 12)
+                .takeWhile(n -> n % 2 == 0)
+                .forEach(n -> System.out.print(n + " "));
     }
 
     @Test
     public void findFirst() throws Exception {
         int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int result = Arrays.stream(numbers)
+                .filter(n -> n == 9)
+                .findFirst()
+                .orElse(-1);
+        System.out.println(result);
     }
 
     @Test
     public void findAny() throws Exception {
         int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 10};
+        int result = Arrays.stream(numbers)
+                .filter(n -> n == 9)
+                .findAny()
+                .orElse(-1);
+        System.out.println(result);
     }
 
     @Test
     public void allMatch() throws Exception {
         int[] even = {2, 4, 6, 8, 10};
+        boolean allMatch = Arrays.stream(even)
+                .allMatch(n -> n % 2 == 0);
+        System.out.println(allMatch);
     }
 
     @Test
     public void anyMatch() throws Exception {
         int[] evenAndOneOdd = {2, 4, 6, 8, 10, 11};
+        boolean anyMatch = Arrays.stream(evenAndOneOdd)
+                .anyMatch(n -> n % 2 == 0);
+        System.out.println(anyMatch);
     }
 
 }
